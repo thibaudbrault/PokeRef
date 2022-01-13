e78888f78a8679879d2f778cda3aa5f77d1af7fa
Thibaud-Brault <thibaud.brault@protonmail.com>
2022-01-13 19:28:27 +0100

    modif

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Nav from './components/Nav'
+import Pokemon from './pages/Pokemon'
 
 export default function Wrapper() {
     return (
         <div className='wrapper' id='wrapper'>
             <Header />
             <Nav />
+            <Pokemon />
             <Footer />
         </div>
     )
---
+++ src/pages/Pokemon.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+
+export default function Pokemon() {
+
+    
+
+    
+    return (
+        <main>
+            <p>Hello</p>
+
+        </main>
+    )
+}
