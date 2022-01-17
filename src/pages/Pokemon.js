b30d78390bc038beb3cf77042e46e2ba29e17896
Thibaud-Brault <thibaud.brault@protonmail.com>
2022-01-17 20:10:11 +0100

    modif

--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react';
-
+import axios from 'axios';
 
 export default function Pokemon() {
 
-    
 
-    
+
     return (
-        <main>
-            <p>Hello</p>
-
-        </main>
+        <div>
+            
+        </div>
     )
 }
