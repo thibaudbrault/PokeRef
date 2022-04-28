2a36a1c820f68f662c3ec4727a4ee2462f7fe43a
Thibaud-Brault <thibaud.brault@protonmail.com>
2022-04-28 20:33:42 +0200

    mini modif

--- src/pages/Abilities/Abilities.js
+++ src/pages/Abilities/Abilities.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import BarWave from 'react-cssfx-loading/lib/BarWave';
 
 import { LeftTitle } from '../../components/BaseStyles/Headings';
