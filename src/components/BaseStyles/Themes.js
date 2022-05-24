0af686ffacaebd9d598907f91ef3f0a0169bc46c
Thibaud-Brault <thibaud.brault@protonmail.com>
2022-05-24 19:51:55 +0200

    small modif

--- src/components/BaseStyles/Themes.js
+++ src/components/BaseStyles/Themes.js
@@ -1,15 +1,5 @@
 import styled from 'styled-components';
 
-export const lightTheme = {
-	main: '#c4c4c4',
-	secondary: '#161616',
-	red: '#cc0000',
-	purple: '#5e318f',
-	toggle: '☾',
-	background:
-		'url("https://www.transparenttextures.com/patterns/asfalt-light.png")',
-};
-
 export const darkTheme = {
 	main: '#161616',
 	secondary: '#c4c4c4',
@@ -18,6 +8,16 @@ export const darkTheme = {
 	toggle: '☀',
 	background:
 		'url("https://www.transparenttextures.com/patterns/asfalt-dark.png")',
+};
+
+export const lightTheme = {
+	main: '#c4c4c4',
+	secondary: '#161616',
+	red: '#cc0000',
+	purple: '#5e318f',
+	toggle: '☾',
+	background:
+		'url("https://www.transparenttextures.com/patterns/asfalt-light.png")',
 };
 
 export const Type = styled.div`
