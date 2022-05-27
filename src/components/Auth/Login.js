fe193648715d5e9ecad908d6b3e1271663858033
Thibaud-Brault <thibaud.brault@protonmail.com>
2022-05-27 20:45:42 +0200

    login form

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AuthBtn, AuthContainer, AuthCreate, AuthInput, AuthPwd, AuthSection } from './StyledAuth'
+import eye from './Images/eye.svg'
+import eyeSlash from './Images/eye-slash.svg'
+import { Link } from 'react-router-dom';
 
 function Login() {
+
+  const [passwordType, setPasswordType] = useState("password");
+  const [passwordInput, setPasswordInput] = useState("");
+  const handlePasswordChange =(evnt)=>{
+      setPasswordInput(evnt.target.value);
+  }
+  const togglePassword =()=>{
+    if(passwordType==="password")
+    {
+     setPasswordType("text")
+     return;
+    }
+    setPasswordType("password")
+  }
+
   return (
-    <section>
-      <h2>Sign In</h2>
-    </section>
+    <AuthSection>
+      <AuthContainer>
+        <form>
+        <h2>Sign In to your account</h2>
+          <AuthInput>
+            <label htmlFor="signInUser">Username</label>
+            <input type="text" name="user" required />
+          </AuthInput>
+          <AuthInput>
+            <label htmlFor="signInEmail">Email address</label>
+            <input type="email" name="email" required />
+          </AuthInput>
+          <AuthInput>
+            <div>
+              <label htmlFor="signInPwd">Password</label>
+            </div>
+            <AuthPwd>
+              <input type={passwordType} value={passwordInput} onChange={handlePasswordChange} name="pwd" required />
+              <button onClick={togglePassword}>
+                { passwordType==="password" ? <img src={eye} alt="Show password" width='25' /> : <img src={eyeSlash} alt="Hide password" width='25' /> }
+              </button>
+            </AuthPwd>
+          </AuthInput>
+          <AuthBtn type="submit">Sign In</AuthBtn>
+        </form>
+      </AuthContainer>
+      <AuthCreate>
+        <p>No account ? <Link to={`/register`}>Create an account</Link></p>
+      </AuthCreate>
+    </AuthSection>
   )
 }
 
---
+++ src/components/Auth/StyledAuth.js
@@ -0,0 +1,106 @@
+import styled from 'styled-components';
+
+export const AuthSection = styled.section`
+    height: 80vh;
+    padding: 5rem 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
+
+export const AuthContainer = styled.div`
+    width: 35%;
+    margin: 0 auto;
+    padding: 2rem;
+    background: ${({ theme }) => theme.secondary};
+    color: ${({ theme }) => theme.main};
+    border-radius: 5px 5px 0 0;
+
+    & h2 {
+        margin-bottom: 2rem;
+        font-size: 3.5rem;
+        text-align: center;
+    }
+`;
+
+export const AuthInput = styled.div`
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    flex-direction: column;
+    margin-bottom: 2rem;
+
+    & label {
+        font-size: 1.5rem;
+        margin: 0 0 0.5rem 0.5rem;
+        text-transform: capitalize;
+    }
+
+    & input {
+        width: 100%;
+        padding: 1rem;
+        background: rgba(255, 255, 255, 0.4);
+        border: 1px solid ${({ theme }) => theme.main};
+        border-radius: 5px;
+        
+        &:focus {
+            border: 1px solid ${({ theme }) => theme.red};
+            outline: none;
+        }
+    }
+`;
+
+export const AuthPwd = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 80% 15%;
+    column-gap: 5%;
+    align-items: center;
+
+    & button {
+        background: none;
+        border: none;
+        cursor: default;
+
+        & img {
+            cursor: pointer;
+            /* filter: invert(84%) sepia(19%) saturate(0%) hue-rotate(195deg) brightness(94%) contrast(83%); */
+        }
+    }
+`;
+
+export const AuthBtn = styled.button`
+    margin: 0 auto;
+    display: flex;
+    padding: 1rem 2rem;
+    font-size: 1.7rem;
+    background: none;
+    border: 1px solid ${({ theme }) => theme.main};
+    color: ${({ theme }) => theme.main};
+    border-radius: 5px;
+`;
+
+export const AuthCreate = styled.div`
+    width: 35%;
+    padding: 2rem;
+    background: ${({ theme }) => theme.main};
+    color: ${({ theme }) => theme.secondary};
+    border: 1px solid ${({ theme }) => theme.secondary};
+    border-radius: 0 0 5px 5px;
+
+    & p {
+        font-size: 1.3rem;
+        text-align: center;
+
+        & a {
+            cursor: pointer;
+            border-bottom: 1px solid transparent;
+            transition: 0.3s ease-in-out;
+
+            &:hover {
+                border-bottom: 1px solid ${({ theme }) => theme.secondary};
+            }
+        }
+    }
+`;
