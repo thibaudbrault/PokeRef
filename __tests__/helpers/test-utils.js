accf80d0955bd766ee8c817e2ad8352eece5b225
Thibaud <thibaud.brault@protonmail.com>
2022-10-17 19:44:31 +0200

    Last commit

--- __tests__/helpers/test-utils.js
+++
@@ -1,6 +0,0 @@
-import { QueryClient, QueryClientProvider } from "react-query";
-
-export const TestQueryProvider = ({ children }) => (
-    <QueryClientProvider client={new QueryClient()}>
-        {children}
-    </QueryClientProvider>);
