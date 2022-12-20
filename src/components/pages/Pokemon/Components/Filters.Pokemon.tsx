58c0d8a4adea6a58658eab46fecf042396a7d95e
Thibaud <thibaud.brault@protonmail.com>
2022-12-20 20:10:10 +0100

    refactor: reduce the size of some code that could be reduced

--- src/components/pages/Pokemon/Components/Filters.Pokemon.tsx
+++ src/components/pages/Pokemon/Components/Filters.Pokemon.tsx
@@ -2,7 +2,8 @@ import { Pokemon } from '@/types/types';
 import React, { Dispatch, SetStateAction, useEffect } from 'react';
 import Autocomplete from '@/components/autocomplete/Autocomplete';
 import { PokedexDropdown, PokedexSearch } from '../Styled.Pokemon';
-import { formFilters, genFilters } from '@/utils/DataArrays';
+import { formFilters, genFilters, typeFilters } from '@/utils/DataArrays';
+import GenerationsMethod from '@/utils/GenerationsMethod';
 
 type Props = {
   pokedex: Pokemon.Pokemon[];
@@ -103,14 +104,7 @@ function Filters({
             }}
           >
             <option value="all">All</option>
-            <option value="gen1">Generation I</option>
-            <option value="gen2">Generation II</option>
-            <option value="gen3">Generation III</option>
-            <option value="gen4">Generation IV</option>
-            <option value="gen5">Generation V</option>
-            <option value="gen6">Generation VI</option>
-            <option value="gen7">Generation VII</option>
-            <option value="gen8">Generation VIII</option>
+            <GenerationsMethod />
           </select>
         </PokedexDropdown>
 
@@ -125,24 +119,11 @@ function Filters({
             }}
           >
             <option value="all">All</option>
-            <option value="bug">Bug</option>
-            <option value="dark">Dark</option>
-            <option value="dragon">Dragon</option>
-            <option value="electric">Electric</option>
-            <option value="fairy">Fairy</option>
-            <option value="fighting">Fighting</option>
-            <option value="fire">Fire</option>
-            <option value="flying">Flying</option>
-            <option value="ghost">Ghost</option>
-            <option value="grass">Grass</option>
-            <option value="ground">Ground</option>
-            <option value="ice">Ice</option>
-            <option value="normal">Normal</option>
-            <option value="poison">Poison</option>
-            <option value="psychic">Psychic</option>
-            <option value="rock">Rock</option>
-            <option value="steel">Steel</option>
-            <option value="water">Water</option>
+            {typeFilters.map(type => (
+              <option value={type.type}>
+                {type.type.charAt(0).toUpperCase() + type.type.slice(1)}
+              </option>
+            ))}
           </select>
         </PokedexDropdown>
       </PokedexSearch>
