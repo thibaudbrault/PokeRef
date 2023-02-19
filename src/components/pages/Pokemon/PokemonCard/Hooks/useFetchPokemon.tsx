dc3e0869039ebd7d512a01db8c8dadbe3cea0a82
Thibaud <thibaud.brault@protonmail.com>
2023-02-19 20:09:36 +0100

    fix: small fix for lacation request

--- src/components/pages/Pokemon/PokemonCard/Hooks/useFetchPokemon.tsx
+++ src/components/pages/Pokemon/PokemonCard/Hooks/useFetchPokemon.tsx
@@ -1,79 +1,79 @@
 import { IPokemon } from '@/types/Pokemon/Pokemon';
 import {
-  getEvolution,
-  getMachines,
-  getMoves,
-  getPokemon,
-  getPokemonLocation,
-  getSpecies,
-  getTypes,
+    getEvolution,
+    getMachines,
+    getMoves,
+    getPokemon,
+    getPokemonLocation,
+    getSpecies,
+    getTypes,
 } from '@/utils/DataFetch';
 import { useQueries, useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 
 export const useFetchPokemon = (name: string) => {
-  const [pokemonId, setPokemonId] = useState<number | null>(null);
-  const [game, setGame] = useState<string | null>(null);
-  const [version, setVersion] = useState<string | null>(null);
+    const [pokemonId, setPokemonId] = useState<number | null>(null);
+    const [game, setGame] = useState<string | null>(null);
+    const [version, setVersion] = useState<string | null>(null);
 
-  const [pokemon, moves, types, machines, location] = useQueries({
-    queries: [
-      {
-        queryKey: [`pokemon`, name],
-        queryFn: () => getPokemon(`https://pokeapi.co/api/v2/pokemon/${name}`),
-        onSuccess: (data: IPokemon) => {
-          setPokemonId(data.id);
-        },
-      },
-      {
-        queryKey: [`moves`, name],
-        queryFn: getMoves,
-      },
-      {
-        queryKey: [`types`, name],
-        queryFn: getTypes,
-      },
-      {
-        queryKey: [`machines`, name],
-        queryFn: getMachines,
-      },
-      {
-        queryKey: [`encounter`, name, game],
+    const [pokemon, moves, types, machines, location] = useQueries({
+        queries: [
+            {
+                queryKey: [`pokemon`, name],
+                queryFn: () => getPokemon(`https://pokeapi.co/api/v2/pokemon/${name}`),
+                onSuccess: (data: IPokemon) => {
+                    setPokemonId(data.id);
+                },
+            },
+            {
+                queryKey: [`moves`, name],
+                queryFn: getMoves,
+            },
+            {
+                queryKey: [`types`, name],
+                queryFn: getTypes,
+            },
+            {
+                queryKey: [`machines`, name],
+                queryFn: getMachines,
+            },
+            {
+                queryKey: [`encounter`, name],
+                queryFn: () =>
+                    getPokemonLocation(
+                        `https://pokeapi.co/api/v2/pokemon/${name}/encounters`,
+                    ),
+            },
+        ],
+    });
+
+    const species = useQuery({
+        queryKey: [`species`, name],
         queryFn: () =>
-          getPokemonLocation(
-            `https://pokeapi.co/api/v2/pokemon/${name}/encounters`,
-          ),
-      },
-    ],
-  });
+            getSpecies(`https://pokeapi.co/api/v2/pokemon-species/${name}`),
+        enabled: !!pokemon.data && pokemon.data.id < 10000,
+    });
 
-  const species = useQuery({
-    queryKey: [`species`, name],
-    queryFn: () =>
-      getSpecies(`https://pokeapi.co/api/v2/pokemon-species/${name}`),
-    enabled: !!pokemon.data && pokemon.data.id < 10000,
-  });
+    const evolutionChainUrl = species.data?.evolution_chain?.url;
 
-  const evolutionChainUrl = species.data?.evolution_chain?.url;
+    const evolution = useQuery({
+        queryKey: [`evolution`, name],
+        queryFn: () => getEvolution(evolutionChainUrl),
+        enabled: !!evolutionChainUrl,
+    });
 
-  const evolution = useQuery({
-    queryKey: [`evolution`, name],
-    queryFn: () => getEvolution(evolutionChainUrl),
-    enabled: !!evolutionChainUrl,
-  });
-
-  return {
-    pokemonId,
-    game,
-    setGame,
-    version,
-    setVersion,
-    pokemon,
-    species,
-    moves,
-    types,
-    machines,
-    location,
-    evolution,
-  };
+    return {
+        pokemonId,
+        game,
+        setGame,
+        version,
+        setVersion,
+        pokemon,
+        species,
+        moves,
+        types,
+        machines,
+        location,
+        evolution,
+    };
 };
