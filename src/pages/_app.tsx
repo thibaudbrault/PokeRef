9dfc98427bde3797bd7c8db552eadd75a7cf0085
Thibaud Brault <thibaud.brault@protonmail.com>
2023-07-12 17:43:03 +0200

    :sparkles: install prisma and next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import PlausibleProvider from 'next-plausible';
 import Head from 'next/head';
 import NextNProgress from 'nextjs-progressbar';
 import { ErrorBoundary, type FallbackProps } from 'react-error-boundary';
 import { Toaster } from 'react-hot-toast';
 
 import { ThemeProvider } from '@/contexts';
-import '@/styles/styles.scss';
 import { Footer, Header, Nav } from '@/modules/layout';
+import '@/styles/styles.scss';
+import { SessionProvider } from 'next-auth/react';
 
 import type { AppProps } from 'next/app';
 
@@ -23,7 +23,7 @@ const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
   );
 };
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -42,21 +42,18 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
       </Head>
       <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <QueryClientProvider client={queryClient}>
-          <ThemeProvider>
-            <PlausibleProvider
-              domain="pokeref.app"
-              enabled={process.env.NODE_ENV === `production`}
-            >
+        <SessionProvider session={session}>
+          <QueryClientProvider client={queryClient}>
+            <ThemeProvider>
               <Toaster />
               <NextNProgress />
               <Header navOpen={navOpen} setNavOpen={setNavOpen} />
               <Nav navOpen={navOpen} setNavOpen={setNavOpen} />
               <Component {...pageProps} />
               <Footer />
-            </PlausibleProvider>
-          </ThemeProvider>
-        </QueryClientProvider>
+            </ThemeProvider>
+          </QueryClientProvider>
+        </SessionProvider>
       </ErrorBoundary>
     </>
   );
