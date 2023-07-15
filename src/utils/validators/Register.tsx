a482ee65a35afde6089887fda1e89b6c83870f42
Thibaud Brault <thibaud.brault@protonmail.com>
2023-07-15 20:26:00 +0200

    :sparkles: create zod validators

---
+++ src/utils/validators/Register.tsx
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+export const RegisterValidator = z
+  .object({
+    name: z
+      .string()
+      .min(3, { message: 'Name must be at least 3 characters long' })
+      .nonempty('A name is required'),
+    email: z.string().email().nonempty('An email is required'),
+    password: z
+      .string()
+      .min(6, { message: 'Your password must be at least 6 characters long' })
+      .nonempty('A password is required'),
+    confirmPassword: z.string().nonempty('You need to confirm your password'),
+  })
+  .refine((value) => value.password === value.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'Passwords must match',
+  });
