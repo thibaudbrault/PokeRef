6ff43dc34663590493bc91212947351fe18df071
Thibaud Brault <thibaud.brault@protonmail.com>
2023-07-17 17:59:01 +0200

    :children_crossing: modify zod error messages

--- src/utils/validators/Login.tsx
+++ src/utils/validators/Login.tsx
@@ -1,6 +1,9 @@
 import { z } from 'zod';
 
 export const LoginValidator = z.object({
-  email: z.string().nonempty(`You need to enter your email adress`),
-  password: z.string().nonempty(`You need to enter your password`),
+  email: z
+    .string()
+    .email({ message: `You need to enter your email address` })
+    .nonempty(),
+  password: z.string().nonempty({ message: `You need to enter your password` }),
 });
--- src/utils/validators/Register.tsx
+++ src/utils/validators/Register.tsx
@@ -5,13 +5,18 @@ export const RegisterValidator = z
     name: z
       .string()
       .min(3, { message: `Name must be at least 3 characters long` })
-      .nonempty(`A name is required`),
-    email: z.string().email().nonempty(`An email is required`),
+      .nonempty(),
+    email: z
+      .string()
+      .email({ message: 'You need to enter a valid email address' })
+      .nonempty(),
     password: z
       .string()
       .min(6, { message: `Your password must be at least 6 characters long` })
-      .nonempty(`A password is required`),
-    confirmPassword: z.string().nonempty(`You need to confirm your password`),
+      .nonempty(),
+    confirmPassword: z
+      .string()
+      .nonempty({ message: `You need to confirm your password` }),
   })
   .refine((value) => value.password === value.confirmPassword, {
     path: [`confirmPassword`],
