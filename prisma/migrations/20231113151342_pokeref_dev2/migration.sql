2dedcf52254286be83e56fd3d907a6532f6741ef
Thibaud Brault <thibaud.brault@protonmail.com>
2023-11-15 15:41:36 +0100

    :card_file_box: add models in prisma schema

---
+++ prisma/migrations/20231113151342_pokeref_dev2/migration.sql
@@ -0,0 +1,66 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[accessToken]` on the table `Session` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `accessToken` to the `Session` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Session` table without a default value. This is not possible if the table is not empty.
+  - Made the column `userId` on table `Session` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- DropIndex
+DROP INDEX "Session_userId_idx";
+
+-- AlterTable
+ALTER TABLE "Session" ADD COLUMN     "accessToken" TEXT NOT NULL,
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ALTER COLUMN "userId" SET NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "providerType" TEXT NOT NULL,
+    "providerId" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refreshToken" TEXT,
+    "accessToken" TEXT,
+    "accessTokenExpires" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationRequest" (
+    "id" TEXT NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "VerificationRequest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_providerId_providerAccountId_key" ON "Account"("providerId", "providerAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationRequest_token_key" ON "VerificationRequest"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationRequest_identifier_token_key" ON "VerificationRequest"("identifier", "token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_accessToken_key" ON "Session"("accessToken");
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/migrations/20231113152222_pokeref_dev3/migration.sql
@@ -0,0 +1,44 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `accessToken` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `accessTokenExpires` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `providerId` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `providerType` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `refreshToken` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Account` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[provider,providerAccountId]` on the table `Account` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `provider` to the `Account` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `type` to the `Account` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Account" DROP CONSTRAINT "Account_userId_fkey";
+
+-- DropIndex
+DROP INDEX "Account_providerId_providerAccountId_key";
+
+-- AlterTable
+ALTER TABLE "Account" DROP COLUMN "accessToken",
+DROP COLUMN "accessTokenExpires",
+DROP COLUMN "createdAt",
+DROP COLUMN "providerId",
+DROP COLUMN "providerType",
+DROP COLUMN "refreshToken",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "access_token" TEXT,
+ADD COLUMN     "expires_at" INTEGER,
+ADD COLUMN     "id_token" TEXT,
+ADD COLUMN     "provider" TEXT NOT NULL,
+ADD COLUMN     "refresh_token" TEXT,
+ADD COLUMN     "scope" TEXT,
+ADD COLUMN     "session_state" TEXT,
+ADD COLUMN     "token_type" TEXT,
+ADD COLUMN     "type" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
